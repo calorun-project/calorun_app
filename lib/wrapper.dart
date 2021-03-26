0f2825aeb92dd42e87a6ac67d1ced9ea90c4ba09
calorun-project <calorun.project@gmail.com>
2021-03-26 11:48:19 +0700

    get data for game

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,5 +1,7 @@
 import 'package:calorun/gameengine/gamedataGE.dart';
 import 'package:calorun/screens/authenticate/prelogin.dart';
+import 'package:calorun/services/database.dart';
+import 'package:calorun/shared/widget_resource.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:calorun/screens/home/home_page.dart';
@@ -13,7 +15,13 @@ class Wrapper extends StatelessWidget {
       return Prepage();
     } else {
       GameData.uid = user;
-      return Home();
+      return FutureBuilder(
+        future: DatabaseServices(uid: user).getGameData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) return circularWaiting();
+          return Home();
+        }
+      );
     }
   }
 }
